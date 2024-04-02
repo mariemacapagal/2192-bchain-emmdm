a542e7ad5e3ecc579d879283606ea859d7c889a0
Marie Macapagal <98503738+mariemacapagal@users.noreply.github.com>
2024-04-02 15:27:33 +0800

    Add files via upload

---
+++ Finals - hello world/contracts/HelloWorld.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.3;
+
+contract HelloWorld {
+
+event UpdatedMessages (string oldStr, string newStr);
+
+string public message;
+
+constructor(string memory initMessage) {
+    message = initMessage;
+}
+
+// A public function that accepts a string argument and updates the message storage variable.
+function update(string memory newMessage) public {
+    string memory oldMsg = message;
+    message = newMessage;
+    emit UpdatedMessages (oldMsg, newMessage);
+}
+}
